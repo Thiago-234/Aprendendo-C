5347539a096cae1027533ab0c71cbf2879717f4a
Thiago-234 <camposthiago2021@gmail.com>
2025-08-11 22:35:46 -0300

    Aprendendo sobre booleanos

---
+++ Boolean/boolean.c
@@ -0,0 +1,9 @@
+// Biblioteca para o uso de booleanos
+#include <stdbool.h>
+// bool-> É a forma como se declara uma váriavel booleana. Retornará 0 ou 1 (falso ou verdadeiro) como em outros casos
+
+int main(){
+    bool maior_idade = false;
+    printf("Retornará 1 caso for maior de idade. Resultado = %d", maior_idade);
+    return 0;
+}
